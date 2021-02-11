2c03cc7411349b36336fda3f324f21cfe6994c9f
amin-alizadeh-16 <aab.alizadeh@gmail.com>
2021-02-11 13:24:22 +0330

    game page

---
+++ lib/pages/game_page.dart
@@ -0,0 +1,137 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_neumorphic/flutter_neumorphic.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+import 'package:lottie/lottie.dart';
+import 'package:pantomim_flutter/theme/app_theme.dart';
+import 'package:pantomim_flutter/theme/colors.dart';
+import 'package:pantomim_flutter/theme/dimense.dart';
+import 'package:pantomim_flutter/widgets/neu_button.dart';
+
+class GamePage extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() => GamePageState();
+}
+
+class GamePageState extends State<GamePage> {
+  @override
+  Widget build(BuildContext context) {
+    var theme = Theme.of(context);
+    return Scaffold(
+        backgroundColor: NeumorphicTheme.accentColor(context),
+        body: Stack(
+          children: [
+            Positioned.fill(
+              right: 30,
+              left: 30,
+              bottom: 0,
+              top: 0,
+              child: Neumorphic(
+                style: NeumorphicStyle(
+                  shape: NeumorphicShape.flat,
+                  boxShape: NeumorphicBoxShape.circle(),
+                  depth: 10,
+                  lightSource: LightSource.topLeft,
+                  color: AppColors.accentColor,
+                ),
+              ),
+            ),
+            Positioned(
+              right: 65,
+              left: 65,
+              bottom: 0,
+              top: 0,
+              child: Neumorphic(
+                style: NeumorphicStyle(
+                  shape: NeumorphicShape.flat,
+                  boxShape: NeumorphicBoxShape.circle(),
+                  depth: 10,
+                  lightSource: LightSource.topLeft,
+                  color: AppColors.accentColor,
+                ),
+              ),
+            ),
+            Positioned(
+              right: 30,
+              left: 30,
+              bottom: 30,
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  NeuButton(() {},
+                      title: "غلط",
+                      color: Color(0xffD32F2F),
+                      align: TextAlign.center),
+                  NeuButton(() {},
+                      svg: "assets/error.svg",
+                      color: Color(0xffF57C00),
+                      align: TextAlign.center),
+                  NeuButton(() {},
+                      title: "درست",
+                      color: Color(0xff388E3C),
+                      align: TextAlign.center),
+                ],
+              ),
+            ),
+            Positioned(
+              top: fullHeight(context) / 1.8,
+              right: fullWidth(context) / 3,
+              left: fullWidth(context) / 3,
+              bottom: fullHeight(context) / 2.5,
+              child: Neumorphic(
+                child: Text(
+                  "تعداد دور",
+                  style: TextStyle(
+                    color: appTheme(context).defaultTextColor,
+                    fontFamily: "aviny",
+                    height: 1.7,
+                    fontSize: subTitleSize(context),
+                  ),
+                  textAlign: TextAlign.center,
+                ),
+                style: NeumorphicStyle(
+                  shape: NeumorphicShape.flat,
+                  boxShape: NeumorphicBoxShape.stadium(),
+                  depth: 10,
+                  lightSource: LightSource.topLeft,
+                  color: AppColors.accentColor,
+                ),
+              ),
+            ),
+            Align(
+              alignment: Alignment(0,-0.1),
+              child: Text("صندلی",
+                style: TextStyle(
+                  color: appTheme(context).baseColor,
+                  fontFamily: "aviny",
+                  fontSize: headline3Size(context),
+                ),
+              ),
+            ),
+            Align(
+              alignment: Alignment(0,-0.8),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Container(
+                    child: Text("01:59",
+                      style: TextStyle(
+                        color: appTheme(context).baseColor,
+                        fontFamily: "aviny",
+                        fontSize: headline3Size(context) / 1.2,
+                      ),
+                    ),
+                    margin: EdgeInsets.only(top: 8),
+                  ),
+                  Container(
+                    width: fullWidth(context) / 10,
+                    height: fullWidth(context) / 10,
+                    child: Lottie.asset('assets/timer.json'),
+                  ),
+                ],
+              ),
+            )
+          ],
+        ));
+  }
+}
--- lib/widgets/neu_button.dart
+++ lib/widgets/neu_button.dart
@@ -1,24 +1,63 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter_neumorphic/flutter_neumorphic.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 import 'package:pantomim_flutter/theme/app_theme.dart';
 import 'package:pantomim_flutter/theme/colors.dart';
 import 'package:pantomim_flutter/theme/dimense.dart';
 
-Widget neuButton(BuildContext context, Function onPress,
-    {String title, IconData icon}) {
-  return NeumorphicButton(
-    onPressed: onPress,
-    style: NeumorphicStyle(
-        color: AppColors.accentColor,
-        intensity: 1.2,
-        depth: 4,
-        boxShape: NeumorphicBoxShape.roundRect(BorderRadius.circular(12))),
-    child: title != null
-        ? Text(title)
-        : Icon(
-            icon,
-            color: appTheme(context).baseColor,
-            size: fullWidth(context) / 18.5,
-          ),
-  );
+class NeuButton extends StatelessWidget {
+  Function onPress;
+  String title;
+  IconData icon;
+  Color color;
+  TextAlign align;
+  String svg;
+
+  NeuButton(this.onPress,
+      {this.icon,
+      this.svg,
+      this.title,
+      this.align: TextAlign.center,
+      this.color});
+
+  @override
+  Widget build(BuildContext context) {
+    return NeumorphicButton(
+      onPressed: onPress,
+      style: NeumorphicStyle(
+          color: AppColors.accentColor,
+          intensity: 1.2,
+          depth: 4,
+          boxShape: NeumorphicBoxShape.roundRect(BorderRadius.circular(12))),
+      child: childNeuButton(context),
+    );
+  }
+
+  Widget childNeuButton(BuildContext context) {
+    if (title != null) {
+      return Text(
+        title,
+        textAlign: align,
+        style: TextStyle(
+          color: color,
+        ),
+      );
+    } else if (icon != null) {
+      return Icon(
+        icon,
+        color: appTheme(context).baseColor,
+        size: fullWidth(context) / 18.5,
+      );
+    } else {
+      return SizedBox(
+        width: fullWidth(context) / 15,
+        height: fullHeight(context) / 20,
+        child: SvgPicture.asset(
+          svg,
+          alignment: Alignment.center,
+          color: Color(0xffF57C00),
+        ),
+      );
+    }
+  }
 }
